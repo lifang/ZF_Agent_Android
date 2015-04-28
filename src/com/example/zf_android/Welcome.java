0969254cd98f0304429e6f53dc387f092c47d6eb
Holin <holin.he@gmail.com>
2015-04-28 22:37:20 +0800

    Guide

--- src/com/example/zf_android/Welcome.java
+++ src/com/example/zf_android/Welcome.java
@@ -1,22 +1,47 @@
 package com.example.zf_android;
 
-import com.posagent.activities.home.Main;
-
-import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 
-public class Welcome extends Activity {
+import com.posagent.activities.BaseActivity;
+import com.posagent.activities.home.GuideActivity;
+import com.posagent.activities.home.LoginActivity;
 
-	@Override
+public class Welcome extends BaseActivity {
+
+    private static final int STOPSPLASH = 0;
+    // time in milliseconds
+    private static final long SPLASHTIME = 1000;
+    private static final String GUIDE_SHOWN_KEY = "shown_v1";
+
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+
+    @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_welcome);
-		
-		Intent i =new Intent(Welcome.this,Main.class);
-		startActivity(i);
-		finish();
-	}
 
+        sharedPreferences = getSharedPreferences("GuideStatus", MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                Intent mainIntent;
+                if (sharedPreferences.getBoolean(GUIDE_SHOWN_KEY, false)) {
+                    mainIntent = new Intent(context, LoginActivity.class);
+                } else {
+                    editor.putBoolean(GUIDE_SHOWN_KEY, true);
+                    editor.commit();
+                    mainIntent = new Intent(context, GuideActivity.class);
+                }
+                Welcome.this.startActivity(mainIntent);
+                Welcome.this.finish();
+            }
+
+        }, SPLASHTIME);
+	}
  
 }
---
+++ src/com/posagent/activities/home/GuideActivity.java
@@ -0,0 +1,169 @@
+package com.posagent.activities.home;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.example.zf_android.R;
+import com.posagent.activities.BaseActivity;
+import com.posagent.utils.ViewHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/***
+ *
+ */
+public class GuideActivity extends BaseActivity implements OnClickListener {
+
+    private ViewPager mViewPager;
+    private List<View> lists = new ArrayList<View>();
+    private ViewPagerAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
+
+        initView();
+
+    }
+
+    private void initView() {
+        mViewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        View item = getLayoutInflater().inflate(R.layout.item, null);
+        ImageView iv = (ImageView)item.findViewById(R.id.image);
+        iv.setImageResource(R.drawable.guide1);
+        lists.add(item);
+
+        item = getLayoutInflater().inflate(R.layout.item, null);
+        iv = (ImageView)item.findViewById(R.id.image);
+        iv.setImageResource(R.drawable.guide2);
+        lists.add(item);
+
+        item = getLayoutInflater().inflate(R.layout.item, null);
+        iv = (ImageView)item.findViewById(R.id.image);
+        iv.setImageResource(R.drawable.guide3);
+        lists.add(item);
+
+        item = getLayoutInflater().inflate(R.layout.item, null);
+        iv = (ImageView)item.findViewById(R.id.image);
+        iv.setImageResource(R.drawable.guide4);
+        lists.add(item);
+
+        adapter = new ViewPagerAdapter(lists);
+
+        mViewPager = (ViewPager) findViewById(R.id.viewPager);
+        mViewPager.setAdapter(adapter);
+        mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            // 当滑动式，顶部的imageView是通过animation缓慢的滑动
+            @Override
+            public void onPageSelected(int idx) {
+                if (idx == lists.size() - 1) {
+                    show("rl_actions");
+                }
+                changDots(idx);
+            }
+
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+            }
+            @Override
+            public void onPageScrollStateChanged(int arg0) {
+
+            }
+        });
+
+
+        findViewById(R.id.tv_register).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(context, Register.class);
+                startActivity(i);
+            }
+        });
+
+        findViewById(R.id.tv_use).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(context, LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.login_linear_login:
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void changDots(int idx) {
+        List<View> views = ViewHelper.getViewsByTag((LinearLayout)findViewById(R.id.ll_dot_container), "dot");
+
+        ImageView iv;
+        for (int i = 0; i < views.size(); i++) {
+            iv = (ImageView)views.get(i);
+            if (i == idx) {
+                iv.setImageResource(R.drawable.dot_press);
+            } else {
+                iv.setImageResource(R.drawable.dot);
+            }
+        }
+
+    }
+
+
+
+
+    public class ViewPagerAdapter extends PagerAdapter {
+
+        List<View> viewLists;
+
+        public ViewPagerAdapter(List<View> lists)
+        {
+            viewLists = lists;
+        }
+
+        //获得size
+        @Override
+        public int getCount() {
+            return viewLists.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View arg0, Object arg1) {
+            return arg0 == arg1;
+        }
+
+        //销毁Item
+        @Override
+        public void destroyItem(View view, int position, Object object)
+        {
+            ((ViewPager) view).removeView(viewLists.get(position));
+        }
+
+        //实例化Item
+        @Override
+        public Object instantiateItem(View view, int position)
+        {
+            ((ViewPager) view).addView(viewLists.get(position), 0);
+            return viewLists.get(position);
+        }
+    }
+}
